d86ef1aa0f67d7b2955d178bb709936f20016e86
Manuel Reich <mr@wundf.net>
2021-04-07 16:40:56 +0200

    Add eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,35 +6,35 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-touch-icon.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
         />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
         <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#5f99d2"
+          rel='mask-icon'
+          href='/favicons/safari-pinned-tab.svg'
+          color='#5f99d2'
         />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#5f99d2" />
+        <link rel='shortcut icon' href='/favicons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#5f99d2' />
         <meta
-          name="msapplication-config"
-          content="/favicons/browserconfig.xml"
+          name='msapplication-config'
+          content='/favicons/browserconfig.xml'
         />
-        <meta name="theme-color" content="#a8c9e8" />
+        <meta name='theme-color' content='#a8c9e8' />
       </Head>
       <Component {...pageProps} />
       <style jsx global>{`
