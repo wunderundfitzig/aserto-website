d01994cc665fd7e87348e492d7639e67336bc807
Manuel Reich <mr@wundf.net>
2021-07-21 16:11:11 +0200

    Add person cards

--- components/person-card.tsx
+++ components/person-card.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from 'react'
+import Image from 'next/image'
 import { Contact, ImageType } from 'lib/types'
 import { LinkedInIcon } from './social-icons'
+import * as colors from 'lib/colors'
 
 type Props = {
   image: ImageType
@@ -8,21 +10,77 @@ type Props = {
 }
 const PersonCard: FunctionComponent<Props> = (props) => {
   return (
-    <div>
-      <address>
-        <h3>{props.contact.name}</h3>
-        <p>{props.contact.role}</p>
-        <p>{props.contact.phone}</p>
-        <a href={`mailto:${props.contact.mail}`}>{props.contact.mail}</a>
-      </address>
-      <div className='social-links'>
-        <a href={props.contact.xing}>
-          <LinkedInIcon color='white' />
-        </a>
-        <a href={props.contact.xing}>
-          <LinkedInIcon color='white' />
-        </a>
+    <div className='person-card'>
+      <Image {...props.image} alt={`portait of ${props.contact.name}`} />
+      <div className='overlay'>
+        <address>
+          <h3>{props.contact.name}</h3>
+          <p>{props.contact.role}</p>
+          <p>{props.contact.phone}</p>
+          <a href={`mailto:${props.contact.mail}`}>{props.contact.mail}</a>
+        </address>
+        <div className='social-links'>
+          {props.contact.xing && (
+            <a href={props.contact.xing}>
+              <LinkedInIcon color='white' />
+            </a>
+          )}
+          {props.contact.linkedIn && (
+            <a href={props.contact.linkedIn}>
+              <LinkedInIcon color='white' />
+            </a>
+          )}
+        </div>
       </div>
+      <style jsx>{`
+        .person-card {
+          position: relative;
+          display: inline-block;
+          font-size: 0.8em;
+        }
+        .overlay {
+          display: none;
+        }
+
+        .person-card:hover .overlay {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          background-color: ${colors.lightBlue};
+          color: white;
+          padding: 2em;
+        }
+
+        address {
+          text-align: center;
+          font-style: normal;
+          font-weight: 200;
+          margin-bottom: 2em;
+        }
+        address h3 {
+          text-transform: uppercase;
+          font-weight: 200;
+          font-size: 1.5em;
+        }
+
+        address p {
+          margin: 0.5em 0;
+        }
+
+        .social-links {
+          display: grid;
+          grid-auto-flow: column;
+          justify-content: center;
+          grid-gap: 2em;
+        }
+
+        .social-links a {
+          width: 30px;
+        }
+      `}</style>
     </div>
   )
 }
--- components/team/das-sind-wir.tsx
+++ components/team/das-sind-wir.tsx
@@ -1,9 +1,108 @@
 import { FunctionComponent } from 'react'
+import { Contact, ImageType } from 'lib/types'
+import PersonCard from 'components/person-card'
+
+const persons: { image: ImageType; contact: Contact }[] = [
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+      linkedIn: 'https://hui.de',
+      xing: 'https://hui.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+  {
+    image: { src: '/images/portraits/jan.jpg', width: 155, height: 195 },
+    contact: {
+      name: 'Jan Blume',
+      role: 'Senor Data Scietist',
+      phone: '0511-4357389',
+      mail: 'blume@aserto.de',
+    },
+  },
+]
 
 const DasSindWir: FunctionComponent = () => {
   return (
     <section className='das-sind-wir'>
       <h2>Das Sind Wir</h2>
+      <div className='persons'>
+        {persons.map((person, idx) => (
+          <PersonCard key={idx} {...person} />
+        ))}
+      </div>
+      <style jsx>{`
+        h2 {
+          margin-top: 3em;
+        }
+
+        .persons {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 1em;
+        }
+
+        .persons > :global(*:nth-child(5)) {
+          margin-left: 100%;
+        }
+      `}</style>
     </section>
   )
 }
