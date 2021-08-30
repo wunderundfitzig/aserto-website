c63a4ee7e65e72cdab66d55c1339ebb68593ed15
Manuel Reich <mr@wundf.net>
2021-08-30 18:13:25 +0200

    Fix image loader for now

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -1,5 +1,5 @@
 import { ImageLoader } from 'next/image'
 
-export const imageLoader: ImageLoader = ({ src, width, quality }) => {
-  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+export const imageLoader: ImageLoader = ({ src }) => {
+  return src
 }
