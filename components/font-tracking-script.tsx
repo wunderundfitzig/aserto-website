9a12e64ff31116b79d3f884eaee9c8cafcf28329
Manuel Reich <mr@wundf.net>
2021-10-01 12:00:25 +0200

    Make font tracking script global

--- components/font-tracking-script.tsx
+++ components/font-tracking-script.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 
 const FontTrackingScript: FunctionComponent = () => {
   return (
-    <style jsx>{`
+    <style jsx global>{`
       @import url('//hello.myfonts.net/count/3fd8e7');
       @import url('https://fast.fonts.net/lt/1.css?apiType=css&c=a343a050-59da-44f4-bede-b3b933046268&fontids=5729734');
     `}</style>
