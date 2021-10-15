78259215f0d174781c0789212ea798315c493b5a
Manuel Reich <mr@wundf.net>
2021-10-15 21:05:52 +0200

    Setup site blueprint in kirby

---
+++ backend/site/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'pju.webhook-field.hooks' => [
+        'netlify_deploy' => [
+            'url' => 'https://api.netlify.com/build_hooks/YOUR_BUILD_HOOK'
+        ]
+    ]
+];
