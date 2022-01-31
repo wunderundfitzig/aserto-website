fe0c96548266aedf69b2a4e3baa88b965ce81f94
Manuel Reich <mr@wundf.net>
2022-01-31 15:24:48 +0100

    Fade in navigation background on scroll

--- frontend/components/navigation-button.tsx
+++ frontend/components/navigation-button.tsx
@@ -1,5 +1,6 @@
 import { breakpoint, minWidth } from 'lib/breakpoints'
-import { FunctionComponent } from 'react'
+import { useIntersectionObserver } from 'lib/use-intersection-observer'
+import { FunctionComponent, useRef } from 'react'
 import { CloseIcon, HamburgerIcon } from './icons'
 
 type Props = {
@@ -7,15 +8,42 @@ type Props = {
   onClick: () => void
 }
 
+const refs: (HTMLButtonElement | null)[] = []
+const SCROLLED_NAV_OFFSET = 25
+
 const NavigationButton: FunctionComponent<Props> = (props) => {
+  const sectionRef = useRef<HTMLButtonElement | null>(null)
+  refs[0] = sectionRef.current
+  const sectionIndex = useIntersectionObserver(refs, {
+    topOffset: () => SCROLLED_NAV_OFFSET,
+  })
+
   return (
-    <button className='navigation-button' onClick={props.onClick}>
+    <button
+      ref={sectionRef}
+      className='navigation-button'
+      onClick={props.onClick}
+    >
       {props.navigationIsOpen ? <CloseIcon /> : <HamburgerIcon />}
-      <svg className='background' viewBox='0 0 100 100'>
-        <rect x={-1000} y={-100} width={100000} height={280} fill='white' />
+      <svg
+        className={`background ${sectionIndex === null ? '' : 'scrolled'}`}
+        viewBox='0 0 100 100'
+      >
+        <rect x={-1000} y={-200} width={100000} height={380} fill='white' />
       </svg>
 
       <style jsx>{`
+        @keyframes move-fade {
+          from {
+            transform: scaleY(2);
+            opacity: 0;
+          }
+          to {
+            transform: scaleY(1);
+            opacity: 1;
+          }
+        }
+
         .navigation-button {
           position: relative;
           background: none;
@@ -33,8 +61,15 @@ const NavigationButton: FunctionComponent<Props> = (props) => {
           width: 1.5rem;
           height: 1.5rem;
           z-index: -1;
+          opacity: 0;
+          transition: opacity ease-out 1s;
           overflow: visible;
           pointer-events: none;
+        }
+
+        .background.scrolled {
+          animation: move-fade ease-in-out 1s;
+          opacity: 1;
         }
 
         @media ${minWidth(breakpoint.l)} {
