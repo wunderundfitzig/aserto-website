aa3778e5a5769cf725e47a81fe8057d29319bab6
Manuel Reich <mr@wundf.net>
2024-02-02 16:46:52 +0100

    Fix next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,10 +5,8 @@ module.exports = withExportImages({
   output: 'export',
   images: {
     loader: 'custom',
-    images: {
-      imageSizes: [384],
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    },
+    imageSizes: [384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   trailingSlash: true,
 })
